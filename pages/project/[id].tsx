913f622a25401926b5b6ab436d8f2fb905292665
Julio Bissoli <juliobissoli33@gamail.com>
2023-05-04 19:28:34 -0300

    Ajustar  imagem na tala de projetos

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,21 +1,39 @@
+import { ArrowLeft } from "@phosphor-icons/react";
 import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const ProjectDetail: NextPage = (props) => {
   const router = useRouter();
 
+  const [imgUrl, setImg] = useState("");
+
+  useEffect(() => {
+    const url = router?.query?.id?.toString() || "";
+    setImg(url);
+    console.log("veio => ", imgUrl);
+  });
+
   return (
     <section>
-      <div className="relative h-[100vh] w-full ">
-        <img src={ Array.isArray(router?.query?.id) ? router?.query?.id[0] : router?.query?.id} className="h-full w-full" />
+      <div className="relative h-[50vh] w-full ">
+        <img src={`/${imgUrl}`} className="h-full w-full" />
         <div className="absolute text-white top-0  w-full h-full opacity-70 bg-gradient-to-t from-transparent to-zinc-900 from-60%">
-          <div
-            className={`w-ful flex justify-start flex-col p-8`}
-          >
-            <h1 className="text-2xl">{router.query.id}</h1>
+          <div className={`w-ful flex justify-start flex-col p-8`}>
+            <header className="w-full ">
+              <Link href={`/`} className="flex gap-2">
+                <ArrowLeft size={22} />
+                Voltar
+              </Link>
+            </header>
           </div>
         </div>
       </div>
+      <section className="p-20 w-full flex justify-center items-center">
+        <h1 className="text-4xl text-zinc-900">{imgUrl}</h1>
+      </section>
     </section>
   );
 };
